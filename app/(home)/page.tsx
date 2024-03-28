568ffc7752bd70c83ca570eb003048848ee3b37e
Adrien Marques <43345482+marcucus@users.noreply.github.com>
2024-03-28 17:40:18 +0100

    s

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import Home from "../components/home/home";
-import Navbar from "../components/navbar/navbar";
 
 const Page = () => {
   return (
     <div className="w-full">
-      <Navbar/>
       <Home />
     </div>
   );
