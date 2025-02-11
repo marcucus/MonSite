06059e4ea6dfef7a4c5a8e7a589b0ac9b04a04ab
Adrien Marques <43345482+marcucus@users.noreply.github.com>
2025-02-12 00:47:12 +0100

    Update footer.tsx

--- app/components/navbar/footer.tsx
+++ app/components/navbar/footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <div className="bg-[#00001f] text-white p-4 border-t-[2px] border-[#47478d] w-full flex items-center justify-between">
       <div className="flex-grow flex justify-center">
-        <p className="mx-auto text-[8px] md:text-[12px]">Copyright 2024 - MARQUES ADRIEN - Tous droits réservés</p>
+        <p className="mx-auto text-[8px] md:text-[12px]">Copyright 2025 - MARQUES ADRIEN - Tous droits réservés</p>
       </div>
       <div className="flex space-x-4">
         <a href='https://www.linkedin.com/in/adrien-marques-755393181/' target='_blank' rel='noopener noreferrer'>
