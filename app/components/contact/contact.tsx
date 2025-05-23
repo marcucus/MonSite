7a92da65218199234afffc9598dedd610a07f5b7
Adrien Marques <43345482+marcucus@users.noreply.github.com>
2025-05-23 10:13:24 +0200

    Update contact.tsx

--- app/components/contact/contact.tsx
+++ app/components/contact/contact.tsx
@@ -56,7 +56,7 @@ const Contact = () => {
           type="submit" 
           className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 
           py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none" 
-          onClick={() => sendMail()}
+{/*           onClick={() => sendMail()} */}
         >
             {isSending === false ? "Envoyer" : "En cours d'envoi..."}
         </button>
